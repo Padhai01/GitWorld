7cc5a7d626ad5c9e069b3c15e21d686ff8c63088
Akshat Pandey <akshatpandey2431048@gmail.com>
2026-09-11 15:53:17 +0530

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import { ToastProvider } from './context/ToastContext.jsx';
+import { SidebarProvider } from './context/SidebarContext.jsx';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <SidebarProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </SidebarProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
